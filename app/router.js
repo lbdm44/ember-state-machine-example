72aae1831d201c6403eb72b0b9d13b9767de3c5c
Lewis Miller <lewis.miller.github@pm.me>
2021-05-13 10:57:45 -0700

    feat: setup pages for differnt configurations

--- app/router.js
+++ app/router.js
@@ -8,4 +8,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('index', { path: '/' });
+  this.route('without-footer');
+  this.route('with-footer');
 });
